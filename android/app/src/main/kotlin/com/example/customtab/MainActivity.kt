e6e7c9901f6dfa72a474d4ae040550278abe45ca
LENV <technicalpriyanshutaru@gmail.com>
2023-03-25 18:02:16 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/customtab/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.customtab
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,105 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.green,
+      ),
+      home:MyCustomTab(),
+    );
+  }
+}
+
+class MyCustomTab extends StatefulWidget {
+  const MyCustomTab({super.key});
+
+  @override
+  State<MyCustomTab> createState() => _MyCustomTabState();
+}
+
+class _MyCustomTabState extends State<MyCustomTab> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Custom Tap"),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            // mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            // ignore: prefer_const_literals_to_create_immutables
+            children: [
+              // ignore: prefer_const_literals_to_create_immutables
+              Container(
+                height: 50,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20), color: Colors.red),
+                child: TabBar(
+                  indicator: BoxDecoration(
+                    color: Colors.red[800],
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  labelColor: Colors.black,
+                  dividerColor: Colors.black,
+                  // ignore: prefer_const_literals_to_create_immutables
+                  tabs: [
+                    Tab(
+                      icon: Icon(
+                        Icons.add_box,
+                        color: Colors.black,
+                      ),
+                    ),
+                    Tab(
+                      icon: Icon(
+                        Icons.video_call,
+                        color: Colors.black,
+                      ),
+                    ),
+                    Tab(
+                      icon: Icon(
+                        Icons.portrait_sharp,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: TabBarView(children: [
+                  // MyPostTab(),
+                  // MyReelsTab(),
+                  // MyTagTab(),
+                ]),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
